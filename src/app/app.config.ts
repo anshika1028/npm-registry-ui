28f9628adfb05b1952ecf696859d7a1af960f346
Anshika Gupta <anshikagupta1028@gmail.com>
2025-03-03 00:47:12 +0800

    remove ssr rendering - carbon do not support it yet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { MarkdownModule, provideMarkdown } from 'ngx-markdown';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import RelativeTime from 'dayjs/plugin/relativeTime';
 import dayjs from 'dayjs';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -19,9 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideAnimations(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
     provideMarkdown(),
   ],
 };
